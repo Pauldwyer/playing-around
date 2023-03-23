7e41c97cb0fa2c3346827ac48b6e925a14a22150
Pauldwyer <pauldwyer55@gmail.com>
2023-03-23 20:02:14 +0000

    basic exercieses

---
+++ testing.py
@@ -0,0 +1,31 @@
+p = "hello india"
+q = 10
+r = 10.1
+
+print(type(p))
+print(type(q))
+print(type(r))
+print(type(12+31j))
+
+var = 3.12
+print(var)
+print(type(var))
+
+var = "this is now a string"
+print(type(var))
+
+bool(False)
+print(bool(False))
+va1 = 0
+print(bool(va1))
+va2 = 11
+print(bool(va2))
+va3 = -2.3
+print(bool(va3))
+
+print(list(range(10)))
+print(range(10))
+print(list(range(10)))
+print(range(1,10,2))
+print(list(range(1,10,2)))
+print(list(range(20,10,-2)))
