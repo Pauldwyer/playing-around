dce074ea68e7b099ed85b53cff81098d6ec19579
Pauldwyer <pauldwyer55@gmail.com>
2023-03-24 21:02:13 +0000

    udemy lecture

---
+++ udemy.py
@@ -0,0 +1,27 @@
+person1, person2, person3 = "Abby", "James", "Lebron"
+
+print(person1)
+print(person2)
+print(person3)
+
+age1, age2 = 14, 40
+
+age3 = age1 + age2
+
+print(age3)
+
+age4 = age1 % age2
+
+print(age4)
+
+myName = "Paul Dwyer"
+firstName = "Paul"
+secondName = "Richard"
+
+fullName = firstName + " " + secondName
+
+print(fullName)
+
+random = "pauldwyerlaksjfdalskfjlsjf"
+print(random[0:9])
+print(random[3:9])
